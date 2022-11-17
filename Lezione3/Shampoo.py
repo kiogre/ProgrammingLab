fe92886a580f12accce1f6967b0a0cdf1ff9eabd
Marco <62170477+kiogre@users.noreply.github.com>
2022-11-17 14:00:25 +0000

    Correzione Lezione 3

--- Lezione3/Shampoo.py
+++ Lezione3/Shampoo.py
@@ -1,8 +1,6 @@
-import csv
-
 def sum_csv(file_name):
     tot = 0;
-    file = csv.open(file_name, 'r')
+    file = open(file_name, 'r')
     for line in file:
         elements = line.split(',')
         if elements[0] != 'Date':
